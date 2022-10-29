e343d9a86f971b91264a5588ed971ba6da263453
angeloqg <angeloqg@hotmail.com>
2022-10-29 20:08:11 -0300

    Implementação do Services

---
+++ MinimalAPI.Delivery/Extensions/ServiceCollectionExtensions.Service.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MinimalAPI.Delivery.Extensions
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors();
+            builder.Services.AddCarter();
+
+            return builder;
+        }
+
+        public static IApplicationBuilder UserServices(this IApplicationBuilder app)
+        {
+            app.UseCors(c =>
+            {
+                c.AllowAnyMethod();
+                c.AllowAnyHeader();
+                c.AllowCredentials();
+                c.SetIsOriginAllowed((Host) => true);
+            });
+            return app;
+        }
+    }
+}
--- MinimalAPI.Delivery/Program.cs
+++ MinimalAPI.Delivery/Program.cs
@@ -3,7 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Configurando o Swagger
 builder.AddOpenAPI();
 
+// Atribuindo a configuração de serviços
+builder.AddServices();
+
 var app = builder.Build();
+
+app.UserServices();
+app.MapCarter();
 app.UseOpenAPI(String.Empty);
 
 app.Run();
