c3234f566e7044aed1c2842a62ae5801470fed2f
angeloqg <angeloqg@hotmail.com>
2022-10-29 19:53:52 -0300

    Configurano o Swagger

--- MinimalAPI.Delivery/GlobalUsing.cs
+++ MinimalAPI.Delivery/GlobalUsing.cs
@@ -1,1 +1,2 @@
 ﻿global using Carter;
+global using MinimalAPI.Delivery.Extensions;
--- MinimalAPI.Delivery/Program.cs
+++ MinimalAPI.Delivery/Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurando o Swagger
+builder.AddOpenAPI();
+
 var app = builder.Build();
+app.UseOpenAPI(String.Empty);
 
 app.Run();
