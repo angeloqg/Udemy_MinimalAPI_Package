4cdc3cb44c68513c5af0056713f165733643720f
angeloqg <angeloqg@hotmail.com>
2022-10-29 19:42:45 -0300

    Criando o DeliveryModule

---
+++ MinimalAPI.Delivery/Module/DeliveryModule.cs
@@ -0,0 +1,12 @@
+﻿using Carter;
+
+namespace MinimalAPI.Delivery.Module
+{
+    public class DeliveryModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+
+        }
+    }
+}
