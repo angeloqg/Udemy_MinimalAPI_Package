32b753ea085783ed72c1562d26a9dc9752355db7
angeloqg <angeloqg@hotmail.com>
2022-10-29 18:32:57 -0300

    Configurando os Endpoints da API Packages

--- MinimalAPI.Package/Module/PackageModule.cs
+++ MinimalAPI.Package/Module/PackageModule.cs
@@ -1,12 +1,30 @@
-﻿using Carter;
+﻿using Dapper;
+using System.Data.SqlClient;
 
 namespace MinimalAPI.Package.Module
 {
     public class PackageModule : ICarterModule
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        public async void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/package", async(SqlConnection db) => 
+                await db.QueryAsync("select * from package"));
+
+            app.MapGet("/package/{code}", async (SqlConnection db, string code) => 
+                await db.QueryFirstAsync("select * from package where code = @code", new { code})
+            );
+
+            app.MapPost("/package", async (SqlConnection db, RegisterPackageDto dto) =>
+            {
+                var newPackage = await db.QueryFirstOrDefaultAsync<RegisterPackageDto>
+                (
+                    @"insert into package(code, country, description)
+                      output inserted.*
+                      values(@code, @country, @description)", dto);
+
+                return Results.Created($"/package/{newPackage.PackageId}", newPackage);
+
+            });
         }
     }
 }
