e5225ae370316ef5ad84d4ea9b85394115dcacbf
angeloqg <angeloqg@hotmail.com>
2022-10-30 10:45:04 -0300

    Implementando os Endpoints

--- MinimalAPI.Delivery/Module/DeliveryModule.cs
+++ MinimalAPI.Delivery/Module/DeliveryModule.cs
@@ -1,10 +1,28 @@
-﻿namespace MinimalAPI.Delivery.Module
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI.Delivery.Module
 {
     public class DeliveryModule : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
+            app.MapGet("/delivery/{packageId}/history", async (int packageId, DeliveryContext db) => 
+                await db.Locations.Where(x => x.Id == packageId).OrderByDescending(x => x.Id).ToArrayAsync());
 
+
+            app.MapPost("/delivery", async (DeliveryContext db, PackageLocation dto) => 
+            {
+                db.Locations.Add(new Location()
+                {
+                    Id = dto.Id,
+                    Latitude = dto.Latitude,
+                    Longitude = dto.Longitude
+                });
+                
+                await db.SaveChangesAsync();
+
+                return Results.Created($"/delivery/{dto.Id}",dto);
+            });
         }
     }
 }
